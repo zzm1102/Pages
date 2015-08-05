16b25cf374d4a3f11a14158173327b5752dc043c
Chimon.Z <zzm_mon@126.com>
2015-08-05 16:29:12 +0800

    添加点击重置和计算

--- flowGame/js/main.js
+++ flowGame/js/main.js
@@ -28,44 +28,69 @@
 //	
 //	$(this).removeClass("run");
 //})
-	
+
+var flowNum = 0, moneyNum = 0;
+
 function moveOn (e, oncomplete, distance, time) {
 	if (typeof e === "string") {e = document.getElementById(e);}
 	if (!time) { time = 1500;}
-	if (!distance) {distance = 450;};
+	if (!distance) {distance = 450;}
 
 	//使用Math.tan作为一个简单的缓动函数来创建非线性动画
 	//一开始比较慢，后面加速
-	var ease = Math.sqrt;
-
 	var start = (new Date()).getTime();
 	animate();
 
 	function animate () {
 		var elapsed = (new Date()).getTime() - start;  //消耗的时间
-		var fraction = elapsed / time;
+		fraction = elapsed / time;
 
 		if (fraction < 1) {
 			var x = distance * Math.tan(fraction*1.2) / 2.58 + 30;  //计算元素的位置
 			e.style.bottom = x + "px";
-			setTimeout(animate, Math.min(25, time-elapsed));
+			var animateExec = setTimeout(animate, Math.min(25, time-elapsed));
+			//添加触摸重置事件
+			e.onclick = function() {
+				//计算
+				fraction.toFixed(2);
+				flowNum += 2 * fraction;
+				moneyNum += 2 * 5 * fraction;
+				$("#flow").html( flowNum.toFixed(2) );
+				$("#money").html( moneyNum.toFixed(2) );
+				//重置
+				fraction = 1;
+				clearTimeout(animateExec);
+				e.style.bottom = "30px";
+			}
 		} else {
 			e.style.bottom = "30px";
-//			if (oncomplete) { oncomplete(e);}
+			if (oncomplete) { oncomplete(e);}
 		}
 	}
 }
 
+var exec, fraction;
 function randomPick() {
 	var randomNum = Math.floor(Math.random() * 4) + 1; //1-4
 	var liId = "app" + randomNum;
-	var elem = document.getElementById(liId);
-	if(moveOn(liId)) return;
-	moveOn(liId, 1000);
-	setTimeout(randomPick,150);
+	var state = $("#"+liId).data("animated");
+	//改变属性避免重复执行
+	if(state == false) {
+		moveOn(liId, null, 450, 2000);
+		$("#"+liId).data("animated", true);
+		
+		setTimeout(function() {
+			$("#"+liId).data("animated", false);
+		}, 2000)
+	} 
+	
+	exec = setTimeout(randomPick,150);
 }
-//randomPick();
+randomPick();
 
-//$("#app1").on("tap", function(){
-//	moveOn("app1", 1000);
-//})
+//停止游戏
+setTimeout(function(){
+	clearTimeout(exec);
+//	$(".mask").removeClass("hide");
+},10000);
+
